3675b38b6f45487edba9359e2b0b9ce06fea8a50
Kerry Knight <kerry@waveriderdesign.com>
2014-02-08 14:40:05 -0500

    Added an additional card 3D transition to example based on Colin Eberhardt’s VCTransitionsLibrary

---
+++ Example/STPTransitionsExample/STPCardTransition.h
@@ -0,0 +1,13 @@
+//
+//  STPCardTransition.h
+//
+//
+//  Created by Kerry Knight on 2/8/14.
+//  Based on Colin Eberhardt's VCTransitionsLibrary https://github.com/ColinEberhardt/VCTransitionsLibrary
+//  Adapted for STPTransitions by Stepan Hruda
+
+#import "STPTransition.h"
+
+@interface STPCardTransition : STPTransition
+
+@end
