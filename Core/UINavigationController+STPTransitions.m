60a6d98bec12fd1b9263402c187f4751b1c36b3b
Kerry Knight <kerry@waveriderdesign.com>
2014-02-09 12:19:45 -0500

    Added whitespace for formatting cleanup of delegate casts

--- Core/UINavigationController+STPTransitions.m
+++ Core/UINavigationController+STPTransitions.m
@@ -11,7 +11,7 @@
                                        reason:@"The navigation controller's delegate has to be a instance of STPTransitionCenter."
                                      userInfo:nil];
     }
-    STPTransitionCenter *center = (STPTransitionCenter*)self.delegate;
+    STPTransitionCenter *center = (STPTransitionCenter *)self.delegate;
     [center setNextPushOrPresentTransition:transition fromViewController:self.topViewController];
     [self pushViewController:viewController animated:YES];
 }
@@ -22,7 +22,7 @@
                                        reason:@"The navigation controller's delegate has to be a instance of STPTransitionCenter."
                                      userInfo:nil];
     }
-    STPTransitionCenter *center = (STPTransitionCenter*)self.delegate;
+    STPTransitionCenter *center = (STPTransitionCenter *)self.delegate;
     [center setNextPopOrDismissTransition:transition fromViewController:self.topViewController];
     return [self popViewControllerAnimated:YES];
 }
